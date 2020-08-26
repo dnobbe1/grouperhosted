c07ec7801e49ed0ed52a52052039b6931cc0f4de
Derek Nobbe <dnobbe@dminc.com>
2020-08-26 12:01:46 -0400

    Updates

---
+++ precache-manifest.44360bb4370add1323150ee6c07f0201.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cc5b46b7e545f06fba2de7e277504c5",
+    "url": "/grouperhosted/index.html"
+  },
+  {
+    "revision": "2d9e291d466e98298a47",
+    "url": "/grouperhosted/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c6ee6d16228b9648754f",
+    "url": "/grouperhosted/static/js/2.c5f6fbb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/grouperhosted/static/js/2.c5f6fbb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d9e291d466e98298a47",
+    "url": "/grouperhosted/static/js/main.05293944.chunk.js"
+  },
+  {
+    "revision": "c057da988d7d1e73e132",
+    "url": "/grouperhosted/static/js/runtime-main.fb404575.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/grouperhosted/static/media/logo.5d5d9eef.svg"
+  }
+]);
